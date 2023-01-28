0e02bf18dc2f1d9f1eff661975aa819b792ffba6
Adam Scanga <sandmanscanga@gmail.com>
2023-01-28 00:19:05 -0500

    Have a functioning proof of work algorithm and functional transactions that can be added on an ad hoc basis.

--- blockchain.py
+++ blockchain.py
@@ -2,9 +2,11 @@
 import json
 import logging
 import logging.config
+from argparse import ArgumentParser, Namespace
 from copy import deepcopy
 from datetime import datetime
 from hashlib import sha256
+from random import choice, randint
 from typing import Dict, List, Union
 
 from config import logger_config
@@ -16,12 +18,16 @@ logger = logging.getLogger("blockchain")
 class Blockchain:
     """The blueprint to the blockchain."""
 
+    target_proof_string = "B00B"
+
     __transaction = Dict[str, Union[str, float]]
     __transactions = List[__transaction]
     __block = Dict[str, Union[str, int, float, __transactions]]
 
-    def __init__(self) -> None:
+    def __init__(self, verbose: bool = True) -> None:
         """Initialize a new instance."""
+
+        self.verbose = verbose
 
         self.chain = []
         self.transactions = []
@@ -57,9 +63,65 @@ class Blockchain:
         self.chain.append(block)
         self.transactions.clear()
 
-        logger.info(f"Mined a new block into the chain with index: {index}")
+        if self.verbose:
+            logger.info(f"Mined new block into the chain with index: {index}")
 
-    def hash_block(self, block: __block) -> str:
+    def add_transaction(self,
+                        sender: str,
+                        recipient: str,
+                        amount: float
+                        ) -> None:
+        """Add a transaction to the pending blockchain transactions."""
+
+        transaction = {
+            "sender": sender,
+            "recipient": recipient,
+            "amount": amount
+        }
+
+        self.transactions.append(transaction)
+
+        if self.verbose:
+            logger.info(f"Added a new transaction with amount {amount} "
+                        f"from {sender} to {recipient}.")
+
+    def proof_of_work(self) -> int:
+        """Calculate the proof of work for a mining operation."""
+
+        previous_proof = self.last_block["proof"]
+        previous_hash = self.hash_block(self.last_block)
+
+        proof = 0
+        while not self.check_proof(previous_proof, previous_hash, proof):
+            proof += 1
+
+        return proof
+
+    def check_proof(self,
+                    previous_proof: int,
+                    previous_hash: str,
+                    proof: int
+                    ) -> bool:
+        """Check to see if the proof is valid."""
+
+        proof_string = f"{previous_proof}{previous_hash}{proof}"
+        proof_hash = sha256(proof_string.encode("utf8")).hexdigest()
+
+        is_valid = False
+        proof_prefix = proof_hash[:len(self.target_proof_string)]
+        if proof_prefix.upper() == self.target_proof_string:
+            if self.verbose:
+                logger.info(f"Proof of work solved with "
+                            f"previous proof {previous_proof}, "
+                            f"previous hash {previous_hash}, and "
+                            f"proof {proof}")
+
+            is_valid = True
+
+        return is_valid
+
+    @staticmethod
+    def hash_block(block: __block) -> str:
         """Generates the SHA256 checksum of a block."""
 
         block_string = json.dumps(block, sort_keys=True)
@@ -69,15 +131,46 @@ class Blockchain:
         return block_hash
 
 
-def main() -> None:
+def get_random_wallets(total: int = 1) -> List[str]:
+    """Generate a random list of wallet addresses."""
+
+    wallet_addresses = []
+    for _ in range(total):
+        wallet_address = sha256(bytes(randint(10000, 99999))).hexdigest()
+        wallet_addresses.append(wallet_address)
+
+    return wallet_addresses
+
+
+def main(args: Namespace) -> None:
     """Execute the main process."""
 
-    blockchain = Blockchain()
+    blockchain = Blockchain(verbose=args.verbose)
 
-    blockchain.mine_block(4365)
+    wallets = get_random_wallets(3)
+
+    for _ in range(randint(5, 10)):  # blocks
+        for _ in range(randint(2, 5)):  # transactions
+            amount = randint(1, 100000) / randint(20, 800)
+            blockchain.add_transaction(sender=choice(wallets),
+                                       recipient=choice(wallets),
+                                       amount=amount)
+
+        proof = blockchain.proof_of_work()
+        blockchain.mine_block(proof=proof)
 
     print(json.dumps(blockchain.chain))
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+
+    parser.add_argument("-v",
+                        "--verbose",
+                        action="store_true",
+                        help="specify verbose flag to increase "
+                             "logging verbosity")
+
+    args = parser.parse_args()
+
+    main(args)
